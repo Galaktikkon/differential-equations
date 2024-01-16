f3456188eaddf00507dba628d67a8d61cde4b320
Marek Malek <malekmarek56@gmail.com>
2024-01-16 20:34:41 +0100

    pipes done

--- elastic_strain/src/main/java/presenter/VisualisationPresenter.java
+++ elastic_strain/src/main/java/presenter/VisualisationPresenter.java
@@ -5,10 +5,13 @@ import javafx.fxml.Initializable;
 import javafx.scene.chart.LineChart;
 import javafx.scene.chart.XYChart;
 import javafx.scene.control.Button;
+import javafx.scene.control.Label;
 import javafx.scene.control.Spinner;
 import model.Solver;
 
+import java.math.RoundingMode;
 import java.net.URL;
+import java.text.DecimalFormat;
 import java.util.ResourceBundle;
 
 public class VisualisationPresenter implements Initializable {
@@ -24,10 +27,17 @@ public class VisualisationPresenter implements Initializable {
     @FXML
     private final Solver solver = new Solver();
 
+    @FXML
+    private Label timeLabel;
+
+    private final DecimalFormat df = new DecimalFormat("#.###");;
+
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
         plot.setCreateSymbols(false);
+        this.df.setRoundingMode(RoundingMode.CEILING);
     }
+
 
     @FXML
     private void generatePlot(){
@@ -43,8 +53,11 @@ public class VisualisationPresenter implements Initializable {
 
     private XYChart.Series<Number,Number> calculateSolution(int n){
 
-
+        long start = System.nanoTime();
         double[] coefficients = solver.solve(n);
+        long stop = System.nanoTime();
+        double elapsed = (stop-start)/1e9;
+        timeLabel.setText(df.format(elapsed)+" [s]");
         XYChart.Series<Number,Number> series = new XYChart.Series<>();
         double h = solver.getH();
         for(int i=0;i<coefficients.length;i++){
